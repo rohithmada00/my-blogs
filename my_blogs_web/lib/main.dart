b7a7f42735af8884929cf1d105d2bd9e3399ea6d
Rohith Reddy Mada <rohithmada00@gmail.com>
2024-05-29 09:27:23 -0400

    [ADD] - basic landing page

--- my_blogs_web/lib/main.dart
+++ my_blogs_web/lib/main.dart
@@ -1,19 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:my_blogs_web/widgets/app_bar.dart';
-import 'package:my_blogs_web/widgets/default_footer.dart';
+// import 'package:my_blogs_web/widgets/default_footer.dart';
 
 void main() async {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        appBar: DefaultAppBar(),
-        body: Center(
-          child: Text(
-            'Hello world!',
-            textDirection: TextDirection.ltr,
+        appBar: const DefaultAppBar(),
+        body: Container(
+          color: Colors.black,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              if (constraints.maxWidth > 600) {
+                // Desktop or larger screen
+                return Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text('Hello Universe...',
+                              style: TextStyle(color: Colors.white)),
+                          Text('I am Rohith...',
+                              style: TextStyle(color: Colors.white)),
+                          Text('species: Homo Sapiens',
+                              style: TextStyle(color: Colors.white)),
+                          Text('age: 24',
+                              style: TextStyle(color: Colors.white)),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text('Image here',
+                            style: TextStyle(color: Colors.white)),
+                        // Replace with your profile image widget
+                        // Image.asset('path_to_your_image.png'),
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                // Mobile or smaller screen
+                return Column(
+                  children: [
+                    Column(
+                      children: const [
+                        Text('Hello Universe...',
+                            style: TextStyle(color: Colors.white)),
+                        Text('I am Rohith...',
+                            style: TextStyle(color: Colors.white)),
+                        Text('species: Homo Sapiens',
+                            style: TextStyle(color: Colors.white)),
+                        Text('age: 24', style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                    Center(
+                      child: Text('Image here',
+                          style: TextStyle(color: Colors.white)),
+                      // Replace with your profile image widget
+                      // Image.asset('path_to_your_image.png'),
+                    ),
+                  ],
+                );
+              }
+            },
           ),
-        ),
-        bottomNavigationBar: DefaultFooter(),
+        ), // bottomNavigationBar: DefaultFooter(),
       ),
     ),
   );
--- my_blogs_web/lib/widgets/app_bar.dart
+++ my_blogs_web/lib/widgets/app_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
   const DefaultAppBar({super.key});
@@ -10,8 +12,21 @@ class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
     // );
 
     return AppBar(
-      title: const Text('Rohith Reddy'),
+      title: Text(
+        'Rohith Reddy Mada',
+        style: GoogleFonts.roboto(),
+      ),
       actions: [
+        TextButton(
+          // style: style,
+          onPressed: () {},
+          child: const Text('Home'),
+        ),
+        TextButton(
+          // style: style,
+          onPressed: () {},
+          child: const Text('Projects'),
+        ),
         TextButton(
           // style: style,
           onPressed: () {},
@@ -20,7 +35,25 @@ class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
         TextButton(
           // style: style,
           onPressed: () {},
-          child: const Text('About'),
+          child: const Text('Contact'),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            FontAwesomeIcons.linkedin,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            FontAwesomeIcons.github,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            FontAwesomeIcons.instagram,
+          ),
         ),
       ],
     );
